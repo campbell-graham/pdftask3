3fb0c8ba8790a196ecc87b7ca6938f5a1f8b861a
Campbell Graham <campbell.graham@gridstone.com.au>
2018-02-12 15:28:15 +1100

    Error handling in place for location services

--- MyLocations/MyLocations/CurrentLocationViewController.swift
+++ MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -13,24 +13,29 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     
     
     var messageLabel: UILabel
-    var latitudeLabel: UILabel
-    var longitudeLabel: UILabel
+    var latitudeValueLabel: UILabel
+    var longitudeValueLabel: UILabel
+    var latitudeTextLabel: UILabel
+    var longitudeTextLabel: UILabel
     var addressLabel: UILabel
     var tagButton: UIButton
     var getButton: UIButton
     let locationManager = CLLocationManager()
     var location: CLLocation!
+    var updatingLocation = false
+    var lastLocationError: Error?
 
     init() {
         //object initialisation
         messageLabel = UILabel()
-        latitudeLabel = UILabel()
-        longitudeLabel = UILabel()
+        latitudeValueLabel = UILabel()
+        longitudeValueLabel = UILabel()
+        latitudeTextLabel = UILabel()
+        longitudeTextLabel = UILabel()
         addressLabel = UILabel()
         tagButton = UIButton(type: .system)
         getButton = UIButton(type: .system)
         super.init(nibName: nil, bundle: nil)
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -48,19 +53,22 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         //button event handling
         getButton.addTarget(self, action: #selector(getLocation), for: .touchUpInside)
         
-        
         //add to view
         view.addSubview(messageLabel)
-        view.addSubview(latitudeLabel)
-        view.addSubview(longitudeLabel)
+        view.addSubview(latitudeValueLabel)
+        view.addSubview(longitudeValueLabel)
+        view.addSubview(latitudeTextLabel)
+        view.addSubview(longitudeTextLabel)
         view.addSubview(addressLabel)
         view.addSubview(tagButton)
         view.addSubview(getButton)
     
         //constraint set up
         messageLabel.translatesAutoresizingMaskIntoConstraints = false
-        latitudeLabel.translatesAutoresizingMaskIntoConstraints = false
-        longitudeLabel.translatesAutoresizingMaskIntoConstraints = false
+        latitudeValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        longitudeValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        latitudeTextLabel.translatesAutoresizingMaskIntoConstraints = false
+        longitudeTextLabel.translatesAutoresizingMaskIntoConstraints = false
         addressLabel.translatesAutoresizingMaskIntoConstraints = false
         tagButton.translatesAutoresizingMaskIntoConstraints = false
         getButton.translatesAutoresizingMaskIntoConstraints = false
@@ -70,18 +78,24 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             messageLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             messageLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             messageLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 26),
-            //latitude label
-            latitudeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            latitudeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            latitudeLabel.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 26),
-            //longitude label
-            longitudeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            longitudeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            longitudeLabel.topAnchor.constraint(equalTo: latitudeLabel.bottomAnchor, constant: 8),
+            //latitude text label
+            latitudeTextLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            latitudeTextLabel.trailingAnchor.constraint(equalTo: latitudeValueLabel.leadingAnchor, constant: -20),
+            latitudeTextLabel.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 26),
+            //longitude text label
+            longitudeTextLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            longitudeTextLabel.trailingAnchor.constraint(equalTo: longitudeValueLabel.leadingAnchor),
+            longitudeTextLabel.topAnchor.constraint(equalTo: latitudeTextLabel.bottomAnchor, constant: 26),
+            //latitude value label
+            latitudeValueLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            latitudeValueLabel.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 26),
+            //longitude value label
+            longitudeValueLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            longitudeValueLabel.topAnchor.constraint(equalTo: latitudeValueLabel.bottomAnchor, constant: 8),
             //address label
             addressLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             addressLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            addressLabel.topAnchor.constraint(equalTo: longitudeLabel.bottomAnchor, constant: 26),
+            addressLabel.topAnchor.constraint(equalTo: longitudeValueLabel.bottomAnchor, constant: 26),
             //tag button
             tagButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             tagButton.topAnchor.constraint(equalTo: addressLabel.bottomAnchor, constant: 26),
@@ -92,8 +106,16 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         
         //text and other visual tweaks
         messageLabel.textAlignment = .center
+        latitudeValueLabel.textAlignment = .right
+        longitudeValueLabel.textAlignment = .right
         tagButton.setTitle("Tag Current Location", for: .normal)
         getButton.setTitle("Get My Location", for: .normal)
+        
+        //default text for labels that do not change
+        latitudeTextLabel.text = "Latitude:"
+        longitudeTextLabel.text = "Longitude:"
+        
+        
         updateLables()
     }
 
@@ -115,19 +137,45 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             return
         }
         
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        locationManager.startUpdatingLocation()
+        startLocationManager()
+        updateLables()
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Did fail with error: \(error)")
+        
+        if (error as NSError).code == CLError.locationUnknown.rawValue {
+            return
+        }
+        
+        lastLocationError = error
+        
+        startLocationManager()
+        updateLables()
+    }
+    
+    func startLocationManager() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            updatingLocation = true
+        }
+    }
+    
+    func stopLocationManager() {
+        if updatingLocation {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            updatingLocation = false
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let newLocation = locations.last!
         print("Did update to location : \(newLocation)")
         location = newLocation
+        lastLocationError = nil
         updateLables()
     }
     
@@ -140,14 +188,28 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     
     func updateLables() {
         if let location = location {
-            latitudeLabel.text = "\(location.coordinate.latitude)"
-            longitudeLabel.text = "\(location.coordinate.longitude)"
+            latitudeValueLabel.text = String(format: "%.8f", location.coordinate.latitude)
+            longitudeValueLabel.text = String(format: "%.8f", location.coordinate.longitude)
             tagButton.isHidden = false
             messageLabel.text = ""
         } else {
-            messageLabel.text = "Tab 'Get My Location' to start"
-            latitudeLabel.text = ""
-            longitudeLabel.text = ""
+            let statusMessage: String
+            if let error = lastLocationError as NSError? {
+                if error.domain == kCLErrorDomain && error.code == CLError.denied.rawValue {
+                    statusMessage = "Location Services Disabled"
+                } else {
+                    statusMessage = "Error Getting Location"
+                }
+            } else if !CLLocationManager.locationServicesEnabled() {
+                statusMessage = "Location Services Disabled"
+            } else if updatingLocation {
+                statusMessage = "Searching..."
+            } else {
+                statusMessage = "Tap 'Get My Location' to Start"
+            }
+            messageLabel.text = statusMessage
+            latitudeValueLabel.text = ""
+            longitudeValueLabel.text = ""
             addressLabel.text = ""
             tagButton.isHidden = true
         }
