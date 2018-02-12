f2386cbfd962e43ef362b7d76f443a5517c3d4a6
Campbell Graham <campbell.graham@gridstone.com.au>
2018-02-12 11:33:41 +1100

    Basic layout of current location controller done

--- MyLocations/MyLocations/CurrentLocationViewController.swift
+++ MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class CurrentLocationViewController: UIViewController {
     
+    
+    var messageLabel: UILabel
+    var latitudeLabel: UILabel
+    var longitudeLabel: UILabel
+    var addressLabel: UILabel
+    var tagButton: UIButton
+    var getButton: UIButton
+    
+
     init() {
+        messageLabel = UILabel()
+        latitudeLabel = UILabel()
+        longitudeLabel = UILabel()
+        addressLabel = UILabel()
+        tagButton = UIButton(type: .system)
+        getButton = UIButton(type: .system)
         super.init(nibName: nil, bundle: nil)
         view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         tabBarItem.image = #imageLiteral(resourceName: "first")
@@ -23,13 +38,65 @@ class CurrentLocationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //add to view
+        view.addSubview(messageLabel)
+        view.addSubview(latitudeLabel)
+        view.addSubview(longitudeLabel)
+        view.addSubview(addressLabel)
+        view.addSubview(tagButton)
+        view.addSubview(getButton)
+    
+        //constraint set up
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        latitudeLabel.translatesAutoresizingMaskIntoConstraints = false
+        longitudeLabel.translatesAutoresizingMaskIntoConstraints = false
+        addressLabel.translatesAutoresizingMaskIntoConstraints = false
+        tagButton.translatesAutoresizingMaskIntoConstraints = false
+        getButton.translatesAutoresizingMaskIntoConstraints = false
 
-        // Do any additional setup after loading the view.
+        NSLayoutConstraint.activate([
+            //message label
+            messageLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            messageLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            messageLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 26),
+            //latitude label
+            latitudeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            latitudeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            latitudeLabel.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 26),
+            //longitude label
+            longitudeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            longitudeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            longitudeLabel.topAnchor.constraint(equalTo: latitudeLabel.bottomAnchor, constant: 8),
+            //address label
+            addressLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            addressLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            addressLabel.topAnchor.constraint(equalTo: longitudeLabel.bottomAnchor, constant: 26),
+            //tag button
+            tagButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            tagButton.topAnchor.constraint(equalTo: addressLabel.bottomAnchor, constant: 26),
+            //get button
+            getButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            getButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -26)
+            ])
+        
+        //text and other visual tweaks
+        messageLabel.textAlignment = .center
+        messageLabel.text = "[user location here]"
+        latitudeLabel.text = "Lattitude: [Latitude goes here]"
+        longitudeLabel.text = "Longitude: [Longitude goes here]"
+        addressLabel.text = "Address goes here"
+        tagButton.setTitle("Tag Current Location", for: .normal)
+        getButton.setTitle("Get My Location", for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getLocation() {
+        
     }
     
 
