d882a7ef1f30c961c0673f4cebd2fe247ea23e17
Campbell Graham <campbell.graham@gridstone.com.au>
2018-02-14 12:18:36 +1100

    Basics for core data added

---
+++ MyLocations/MyLocations/Location+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Campbell Graham on 14/2/18.
+//  Copyright © 2018 someIndustry. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
